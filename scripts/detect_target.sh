51223be402abad7445c4a987c961c53788acb1e1
mahadev <mahadev@rocky9-base.lab>
2026-06-16 22:26:36 +0530

    Fix Jenkins AWS CLI and SSH paths

--- scripts/detect_target.sh
+++ scripts/detect_target.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ACTIVE=$(aws elbv2 describe-listeners \
+ACTIVE=$(/usr/local/bin/aws elbv2 describe-listeners \
   --region ap-south-1 \
   --listener-arns arn:aws:elasticloadbalancing:ap-south-1:938504081144:listener/app/blue-green-alb/7b8d1e8296bffc47/aeee5658472b3af6 \
   --query 'Listeners[0].DefaultActions[0].TargetGroupArn' \
--- scripts/switch_green.sh
+++ scripts/switch_green.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-aws elbv2 modify-listener \
+/usr/local/bin/aws elbv2 modify-listener \
   --region ap-south-1 \
   --listener-arn arn:aws:elasticloadbalancing:ap-south-1:938504081144:listener/app/blue-green-alb/7b8d1e8296bffc47/aeee5658472b3af6 \
   --default-actions Type=forward,TargetGroupArn=arn:aws:elasticloadbalancing:ap-south-1:938504081144:targetgroup/green-tg/852e4e664f728010
